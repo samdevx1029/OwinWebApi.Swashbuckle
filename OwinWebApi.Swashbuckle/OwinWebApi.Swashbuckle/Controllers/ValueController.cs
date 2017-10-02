2261b2b9eeb7001f0a531dfa0230634604a285ac
Sam Shrestha <sshrestha@I-360.LOCAL>
2017-10-02 10:27:39 -0400

    initial

---
+++ OwinWebApi.Swashbuckle/OwinWebApi.Swashbuckle/Controllers/ValueController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OwinWebApi.Swashbuckle.Controllers
+{
+    public class ValueController : ApiController
+    {
+        // GET: api/Value
+        /// <summary>
+        /// Get All values.
+        /// </summary>
+        /// <returns>All values.</returns>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Value/5
+        /// <summary>
+        /// Get a value based on an ID.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A value.</returns>
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Value
+        /// <summary>
+        /// Insert a value.
+        /// </summary>
+        /// <param name="value">value to insert</param>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Value/5
+        /// <summary>
+        /// Update a value.
+        /// </summary>
+        /// <param name="id">Id to find value.</param>
+        /// <param name="value">value to update with.</param>
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Value/5
+        /// <summary>
+        /// Delete a value.
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ OwinWebApi.Swashbuckle/OwinWebApi.Swashbuckle/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using OwinWebApi.Swashbuckle;
+using Swashbuckle.Application;
+using System;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace OwinWebApi.Swashbuckle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+
+            // Configure routing
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("DefaultApi", "{controller}/{id}",
+                defaults: new { controller = "Value", id = RouteParameter.Optional }
+                );
+
+            // Configure Swashbuckle (after installing Swashbuckle.Core)
+            config
+                .EnableSwagger(c =>
+                {
+                    c.SingleApiVersion("v1", "Crimson API");
+                    c.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}\bin\OwinWebApiSwashbuckle.xml");
+                    c.PrettyPrint();
+                })
+                .EnableSwaggerUi();
+
+            // Start using the main Owin middleware (after all pipeline has been set up as desired above)
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
